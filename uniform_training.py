7f0dee91b5078f71e653b7183611a3d01681d00f
JiayangWang0225 <wjywitness@gmail.com>
2021-09-09 10:18:17 -0700

    dataset load

--- uniform_training.py
+++ uniform_training.py
@@ -1,5 +1,21 @@
 # %%
+import pathlib
 import torch
+import fastmri
 from fastmri.models import Unet
-# %%
+from fastmri.data import transforms, mri_data
+# %% data loader
+def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
+    # Transform the kspace to tensor format
+    kspace = transforms.to_tensor(kspace)
+    return kspace
 
+dataset = mri_data.SliceDataset(
+    root=pathlib.Path(
+      '/home/wjy/Project/fastmri_dataset/multicoil_test'
+    ),
+    transform=data_transform,
+    challenge='multicoil'
+)
+
+# %% 
