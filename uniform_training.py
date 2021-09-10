426e45da5301ad755f97489b1a1a4121bc9e145f
JiayangWang0225 <wjywitness@gmail.com>
2021-09-09 23:59:59 -0700

    v3_sep9

--- uniform_training.py
+++ uniform_training.py
@@ -13,8 +13,8 @@ def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
     return kspace
 
 train_data = mri_data.SliceDataset(
-    #root=pathlib.Path(  '/home/wjy/Project/fastmri_dataset/multicoil_test'),
-    root = pathlib.path('/project/jhaldar_118/fastMRI_dataset/Brain/MultiCoil/multicoil_val/T2/'),
+    #root=pathlib.Path('/home/wjy/Project/fastmri_dataset/multicoil_test/T2/'),
+    root = pathlib.Path('/project/jhaldar_118/fastMRI_dataset/Brain/MultiCoil/multicoil_val/T2/'),
     transform=data_transform,
     challenge='multicoil'
 )
