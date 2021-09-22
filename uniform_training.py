9eff8cdc38fa037f614ff3bb79e1024a1fc226aa
JiayangWang0225 <wjywitness@gmail.com>
2021-09-22 11:18:51 -0700

    GPU test

--- uniform_training.py
+++ uniform_training.py
@@ -78,19 +78,18 @@ recon_model = Unet(
 
 
 # %% GPU 
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-#if torch.cuda.is_available() == False:
-#    batch_size = 1
-#    print("Let's use",device)
-#else:
-#    print("Let's use", torch.cuda.device_count(), "GPUs!")
-#    batch_size = torch.cuda.device_count()
-    #sample_model = torch.nn.DataParallel(sample_model)
-    #recon_model = torch.nn.DataParallel(recon_model)
-    #toIm = torch.nn.DataParallel(toIm)
-device = torch.device("cpu")
-batch_size = 1
+if torch.cuda.is_available() == False:
+    batch_size = 1
+    print("Let's use",device)
+else:
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    batch_size = torch.cuda.device_count()
+    sample_model = torch.nn.DataParallel(sample_model)
+    recon_model = torch.nn.DataParallel(recon_model)
+    toIm = torch.nn.DataParallel(toIm)
+
 train_dataloader = torch.utils.data.DataLoader(train_data,batch_size,shuffle=True)
 sample_model.to(device)
 recon_model.to(device)
@@ -108,18 +107,17 @@ for epoch in range(max_epochs):
     print("epoch:",epoch)
     batch_count = 0
     for train_batch in train_dataloader:
-        print(train_batch.size())
-        if train_batch.size(1)!=16 or train_batch.size(2)!=640 or train_batch.size(3)!=320:
+        #print(train_batch.size())
+        if train_batch.size(1)!=16 or train_batch.size(2)!=768 or train_batch.size(3)!=396:
             continue
-    
         batch_count = batch_count + 1
-        #print("batch:",batch_count)
+        
         train_batch.to(device)
+        
         image_noise = sample_model(train_batch).to(device)
-        #print(image_noise.device())
         recon = recon_model(image_noise)
-        #recon = recon_model(sample_model(train_batch))
         ground_truth = toIm(train_batch)
+
         loss = NRMSE_loss(recon,ground_truth)
         if batch_count%10 == 0:
             print("batch:",batch_count,"train loss:",loss.item(),"Original NRMSE:", NRMSE_loss(image_noise,ground_truth))
