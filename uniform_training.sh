2e7b4d8f63c0e37d81be843b35cca2abdb3164c4
JiayangWangUSC <jiayangw@usc.edu>
2021-09-23 12:17:50 -0700

    uniform gpu

--- uniform_training.sh
+++ uniform_training.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16GB
@@ -6,5 +9,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jiayangw@usc.edu
 #SBATCH --account=jhaldar_118
+
+module purge
+module load gcc/8.3.0
+module load cuda/11.2.0
 
 python /project/jhaldar_118/jiayangw/OptSamp/uniform_training.py
