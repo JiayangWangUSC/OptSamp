7810489167c1c5b553f5dd33340a6638ab69c300
JiayangWangUSC <jiayangw@usc.edu>
2021-09-23 11:00:29 -0700

    gpu test

---
+++ gpu_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jiayangw@usc.edu
+#SBATCH --account=jhaldar_118
+
+python /project/jhaldar_118/jiayangw/OptSamp/gpu_test.py
