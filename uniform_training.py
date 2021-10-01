db3a5c6615d40e9d2195614debeb3142f71fabd2
JiayangWang0225 <wjywitness@gmail.com>
2021-09-30 18:35:14 -0700

    correct coil combination

--- uniform_training.py
+++ uniform_training.py
@@ -117,7 +117,7 @@ toIm.to(device)
 recon_optimizer = optim.RMSprop(recon_model.parameters(),lr=1e-3)
 
 # %% training
-max_epochs = 3
+max_epochs = 10
 val_loss = torch.zeros(max_epochs)
 for epoch in range(max_epochs):
     print("epoch:",epoch+1)
