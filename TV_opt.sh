707c13414ed9f04a4db43fd4e09f4b17267031c8
JiayangWang0225 <wjywitness@gmail.com>
2021-10-14 23:22:12 -0700

    TV

--- TV_opt.sh
+++ TV_opt.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=16GB
-#SBATCH --time=36:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jiayangw@usc.edu
 #SBATCH --account=jhaldar_118
