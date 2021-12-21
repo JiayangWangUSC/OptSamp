9d4ef5a5d50867651895d8bff7697a4b35c51c79
JiayangWang <jiayangwQ@usc.edu>
2021-12-20 16:55:20 -0800

    varnet

--- uniform_training.py
+++ uniform_training.py
@@ -1,11 +1,289 @@
+
 # %%
-import pathlib
-import torch
-import torch.optim as optim
+import math
+from typing import List, Tuple, Optional
+
 import fastmri
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from fastmri.data import transforms
 from fastmri.models import Unet
-from fastmri.data import transforms, mri_data
-from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+
+import pathlib
+import torch.optim as optim
+from fastmri.data import  mri_data
+# %%
+class NormUnet(nn.Module):
+    """
+    Normalized U-Net model.
+    This is the same as a regular U-Net, but with normalization applied to the
+    input before the U-Net. This keeps the values more numerically stable
+    during training.
+    """
+
+    def __init__(
+        self,
+        chans: int,
+        num_pools: int,
+        in_chans: int = 2,
+        out_chans: int = 2,
+        drop_prob: float = 0.0,
+    ):
+        """
+        Args:
+            chans: Number of output channels of the first convolution layer.
+            num_pools: Number of down-sampling and up-sampling layers.
+            in_chans: Number of channels in the input to the U-Net model.
+            out_chans: Number of channels in the output to the U-Net model.
+            drop_prob: Dropout probability.
+        """
+        super().__init__()
+
+        self.unet = Unet(
+            in_chans=in_chans,
+            out_chans=out_chans,
+            chans=chans,
+            num_pool_layers=num_pools,
+            drop_prob=drop_prob,
+        )
+
+    def complex_to_chan_dim(self, x: torch.Tensor) -> torch.Tensor:
+        b, c, h, w, two = x.shape
+        assert two == 2
+        return x.permute(0, 4, 1, 2, 3).reshape(b, 2 * c, h, w)
+
+    def chan_complex_to_last_dim(self, x: torch.Tensor) -> torch.Tensor:
+        b, c2, h, w = x.shape
+        assert c2 % 2 == 0
+        c = c2 // 2
+        return x.view(b, 2, c, h, w).permute(0, 2, 3, 4, 1).contiguous()
+
+    def norm(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # group norm
+        b, c, h, w = x.shape
+        x = x.view(b, 2, c // 2 * h * w)
+
+        mean = x.mean(dim=2).view(b, 2, 1, 1)
+        std = x.std(dim=2).view(b, 2, 1, 1)
+
+        x = x.view(b, c, h, w)
+
+        return (x - mean) / std, mean, std
+
+    def unnorm(
+        self, x: torch.Tensor, mean: torch.Tensor, std: torch.Tensor
+    ) -> torch.Tensor:
+        return x * std + mean
+
+    def pad(
+        self, x: torch.Tensor
+    ) -> Tuple[torch.Tensor, Tuple[List[int], List[int], int, int]]:
+        _, _, h, w = x.shape
+        w_mult = ((w - 1) | 15) + 1
+        h_mult = ((h - 1) | 15) + 1
+        w_pad = [math.floor((w_mult - w) / 2), math.ceil((w_mult - w) / 2)]
+        h_pad = [math.floor((h_mult - h) / 2), math.ceil((h_mult - h) / 2)]
+        # TODO: fix this type when PyTorch fixes theirs
+        # the documentation lies - this actually takes a list
+        # https://github.com/pytorch/pytorch/blob/master/torch/nn/functional.py#L3457
+        # https://github.com/pytorch/pytorch/pull/16949
+        x = F.pad(x, w_pad + h_pad)
+
+        return x, (h_pad, w_pad, h_mult, w_mult)
+
+    def unpad(
+        self,
+        x: torch.Tensor,
+        h_pad: List[int],
+        w_pad: List[int],
+        h_mult: int,
+        w_mult: int,
+    ) -> torch.Tensor:
+        return x[..., h_pad[0] : h_mult - h_pad[1], w_pad[0] : w_mult - w_pad[1]]
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if not x.shape[-1] == 2:
+            raise ValueError("Last dimension must be 2 for complex.")
+
+        # get shapes for unet and normalize
+        x = self.complex_to_chan_dim(x)
+        x, mean, std = self.norm(x)
+        x, pad_sizes = self.pad(x)
+
+        x = self.unet(x)
+
+        # get shapes back and unnormalize
+        x = self.unpad(x, *pad_sizes)
+        x = self.unnorm(x, mean, std)
+        x = self.chan_complex_to_last_dim(x)
+
+        return x
+
+
+class SensitivityModel(nn.Module):
+    """
+    Model for learning sensitivity estimation from k-space data.
+    This model applies an IFFT to multichannel k-space data and then a U-Net
+    to the coil images to estimate coil sensitivities. It can be used with the
+    end-to-end variational network.
+    """
+
+    def __init__(
+        self,
+        chans: int,
+        num_pools: int,
+        in_chans: int = 2,
+        out_chans: int = 2,
+        drop_prob: float = 0.0,
+        mask_center: bool = True,
+    ):
+        """
+        Args:
+            chans: Number of output channels of the first convolution layer.
+            num_pools: Number of down-sampling and up-sampling layers.
+            in_chans: Number of channels in the input to the U-Net model.
+            out_chans: Number of channels in the output to the U-Net model.
+            drop_prob: Dropout probability.
+            mask_center: Whether to mask center of k-space for sensitivity map
+                calculation.
+        """
+        super().__init__()
+        self.mask_center = mask_center
+        self.norm_unet = NormUnet(
+            chans,
+            num_pools,
+            in_chans=in_chans,
+            out_chans=out_chans,
+            drop_prob=drop_prob,
+        )
+
+    def batch_chans_to_chan_dim(self, x: torch.Tensor, batch_size: int) -> torch.Tensor:
+        bc, _, h, w, comp = x.shape
+        c = bc // batch_size
+        return x.view(batch_size, c, h, w, comp)
+    
+    def chans_to_batch_dim(self, x: torch.Tensor) -> Tuple[torch.Tensor, int]:
+        b, c, h, w, comp = x.shape
+        return x.view(b * c, 1, h, w, comp), b
+
+    def divide_root_sum_of_squares(self, x: torch.Tensor) -> torch.Tensor:
+        return x / fastmri.rss_complex(x, dim=1).unsqueeze(-1).unsqueeze(1)
+
+    def forward(
+        self,
+        masked_kspace: torch.Tensor
+    ) -> torch.Tensor:
+
+        mask = torch.zeros_like(masked_kspace)
+        mask[:,:,:,torch.arange(186,210),:] = 1
+        masked_kspace = torch.mul(mask,masked_kspace)
+        # convert to image space
+        images, batches = self.chans_to_batch_dim(fastmri.ifft2c(masked_kspace))
+
+        # estimate sensitivities
+        return self.divide_root_sum_of_squares(
+            self.batch_chans_to_chan_dim(self.norm_unet(images), batches)
+        )
+
+
+class VarNet(nn.Module):
+    """
+    A full variational network model.
+    This model applies a combination of soft data consistency with a U-Net
+    regularizer. To use non-U-Net regularizers, use VarNetBlock.
+    """
+
+    def __init__(
+        self,
+        num_cascades: int = 12,
+        sens_chans: int = 8,
+        sens_pools: int = 4,
+        chans: int = 18,
+        pools: int = 4,
+        mask_center: bool = True,
+    ):
+        """
+        Args:
+            num_cascades: Number of cascades (i.e., layers) for variational
+                network.
+            sens_chans: Number of channels for sensitivity map U-Net.
+            sens_pools Number of downsampling and upsampling layers for
+                sensitivity map U-Net.
+            chans: Number of channels for cascade U-Net.
+            pools: Number of downsampling and upsampling layers for cascade
+                U-Net.
+            mask_center: Whether to mask center of k-space for sensitivity map
+                calculation.
+        """
+        super().__init__()
+
+        self.sens_net = SensitivityModel(
+            chans=sens_chans,
+            num_pools=sens_pools,
+            mask_center=mask_center,
+        )
+        self.cascades = nn.ModuleList(
+            [VarNetBlock(NormUnet(chans, pools)) for _ in range(num_cascades)]
+        )
+
+    def forward(
+        self,
+        masked_kspace: torch.Tensor,
+        mask: torch.Tensor
+    ) -> torch.Tensor:
+        sens_maps = self.sens_net(masked_kspace)
+        kspace_pred = masked_kspace.clone()
+
+        for cascade in self.cascades:
+            kspace_pred = cascade(kspace_pred, masked_kspace, mask, sens_maps)
+
+        return fastmri.rss(fastmri.complex_abs(fastmri.ifft2c(kspace_pred)), dim=1)
+
+
+class VarNetBlock(nn.Module):
+    """
+    Model block for end-to-end variational network.
+    This model applies a combination of soft data consistency with the input
+    model as a regularizer. A series of these blocks can be stacked to form
+    the full variational network.
+    """
+
+    def __init__(self, model: nn.Module):
+        """
+        Args:
+            model: Module for "regularization" component of variational
+                network.
+        """
+        super().__init__()
+
+        self.model = model
+        self.dc_weight = nn.Parameter(torch.ones(1))
+
+    def sens_expand(self, x: torch.Tensor, sens_maps: torch.Tensor) -> torch.Tensor:
+        return fastmri.fft2c(fastmri.complex_mul(x, sens_maps))
+
+    def sens_reduce(self, x: torch.Tensor, sens_maps: torch.Tensor) -> torch.Tensor:
+        return fastmri.complex_mul(
+            fastmri.ifft2c(x), fastmri.complex_conj(sens_maps)
+        ).sum(dim=1, keepdim=True)
+
+    def forward(
+        self,
+        current_kspace: torch.Tensor,
+        ref_kspace: torch.Tensor,
+        mask: torch.Tensor,
+        sens_maps: torch.Tensor,
+    ) -> torch.Tensor:
+        zero = torch.zeros(1, 1, 1, 1, 1).to(current_kspace)
+        soft_dc = torch.where(mask, current_kspace - ref_kspace, zero) * self.dc_weight
+        model_term = self.sens_expand(
+            self.model(self.sens_reduce(current_kspace, sens_maps)), sens_maps
+        )
+
+        return current_kspace - soft_dc - model_term
+
+#from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 # %% data loader
 def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
     # Transform the kspace to tensor format
@@ -40,24 +318,13 @@ class Sample(torch.nn.Module):
         self.sigma = sigma
 
     def forward(self,kspace):
-
         noise = self.sigma*torch.randn_like(kspace)
         kspace_noise = kspace + torch.div(noise,torch.sqrt(self.mask).unsqueeze(0).unsqueeze(1).unsqueeze(3).unsqueeze(0).repeat(kspace.size(0),16,384,1,2))  # need to reshape mask        image = fastmri.ifft2c(kspace_noise)
-        image = fastmri.ifft2c(kspace_noise)
-        image = torch.sqrt(torch.sum(torch.sum(torch.mul(image,image),4),1)).unsqueeze(1)
-        #image = transforms.normalize(image,glob_mean,glob_std,1e-11)
-        return image
+        return kspace_noise
 
-class toImage(torch.nn.Module): 
-
-    def __init__(self):
-        super().__init__()
-
-    def forward(self,kspace):
-        image = fastmri.ifft2c(kspace)
-        image = torch.sqrt(torch.sum(torch.sum(torch.mul(image,image),4),1)).unsqueeze(1)
-        #image = transforms.normalize(image,glob_mean,glob_std,1e-11)
-        return image
+def toIm(kspace): 
+    image = fastmri.rss(fastmri.complex_abs(fastmri.ifft2c(kspace)), dim=1)
+    return image
 # %% mask _extraction
 def support_extraction(Batch):
     result = Batch.clone()
@@ -141,18 +408,26 @@ sigma = 0.4
 print("noise level:", sigma)
 sample_model = Sample(sigma,factor)
 
-toIm = toImage()
 
 # %% unet loader
-recon_model = Unet(
-  in_chans = 1,
-  out_chans = 1,
-  chans = 32,
-  num_pool_layers = 4,
-  drop_prob = 0.0
+#recon_model = Unet(
+#  in_chans = 1,
+#  out_chans = 1,
+#  chans = 32,
+#  num_pool_layers = 4,
+#  drop_prob = 0.0
+#)
+
+recon_model = VarNet(
+    num_cascades = 12,
+    sens_chans = 8,
+    sens_pools = 4,
+    chans = 18,
+    pools = 4
 )
 
-recon_model = torch.load('/project/jhaldar_118/jiayangw/OptSamp/uniform_model_selfloss_noise'+str(sigma))
+
+#recon_model = torch.load('/project/jhaldar_118/jiayangw/OptSamp/uniform_model_selfloss_noise'+str(sigma))
 
 # %% GPU 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -162,7 +437,6 @@ train_dataloader = torch.utils.data.DataLoader(train_data,batch_size,shuffle=Tru
 
 sample_model.to(device)
 recon_model.to(device)
-toIm.to(device)
 
 
 # %% gradient computation
@@ -187,31 +461,31 @@ for n2 in range(N2):
 
 def GradMap(Batch,support,D1,D2):
     gradmap = support.clone()
-    N1 = Batch.size(2)
-    N2 = Batch.size(3)
+    N1 = Batch.size(1)
+    N2 = Batch.size(2)
     for batch in range(Batch.size(0)):
-        Im = Batch[batch,0,:,:].squeeze()
-        sp = support[batch,0,:,:].squeeze()
+        Im = Batch[batch,:,:].squeeze()
+        sp = support[batch,:,:].squeeze()
         G1 = torch.matmul(D1,Im)
         G2 = torch.matmul(Im,D2)
         G = torch.sqrt(torch.mul(G1,G1)+torch.mul(G2,G2))
         G = torch.reshape(torch.mul(G,sp),(-1,))
         sorted,_ = torch.sort(G,descending=True)
         th = sorted[round(torch.sum(sp).item()*0.3)]
-        gradmap[batch,0,:,:] = torch.reshape(torch.ge(G,th),(N1,N2))
+        gradmap[batch,:,:] = torch.reshape(torch.ge(G,th),(N1,N2))
 
     return gradmap
 
 # %% optimizer
-recon_optimizer = optim.RMSprop(recon_model.parameters(),lr=1e-3)
+recon_optimizer = optim.Adam(recon_model.parameters(),lr=3e-3)
 #Loss = torch.nn.MSELoss()
 L1Loss = torch.nn.L1Loss()
 L2Loss = torch.nn.MSELoss()
 #ms_ssim_module = MS_SSIM(data_range=255, size_average=True, channel=1)
 
 # %% training
-max_epochs = 10
-val_loss = torch.zeros(max_epochs)
+max_epochs = 1
+#val_loss = torch.zeros(max_epochs)
 for epoch in range(max_epochs):
     print("epoch:",epoch+1)
 
@@ -225,8 +499,9 @@ for epoch in range(max_epochs):
         support = torch.ge(gt,0.1*torch.max(gt))
         gradmap = GradMap(gt,support,D1,D2)
         
-        image_noise = sample_model(train_batch).to(device)
-        recon = recon_model(image_noise)
+        kspace_noise = sample_model(train_batch).to(device)
+        mask = torch.ones_like(kspace_noise).to(bool)
+        recon = recon_model(kspace_noise, mask)
         loss = L2Loss(torch.mul(recon.to(device),support.to(device)),torch.mul(gt.to(device),support.to(device))) + L1Loss(torch.mul(recon.to(device),gradmap.to(device)),torch.mul(gt.to(device),gradmap.to(device)))
         #loss = 1- ms_ssim_module(recon*25,recon*25)
 
@@ -261,4 +536,6 @@ for epoch in range(max_epochs):
 #        print("epoch:",epoch+1,"validation Loss:",val_loss[epoch])
 
    # torch.save(val_loss,"./uniform_model_val_loss_noise"+str(sigma))
-    torch.save(recon_model,"./uniform_model_selfloss_noise"+str(sigma))
+    torch.save(recon_model,"./uniform_varnet_selfloss_noise"+str(sigma))
+
+# %%
