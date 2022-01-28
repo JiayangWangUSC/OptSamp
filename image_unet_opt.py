8b89bc9bed61710fd9bbc9b7752995af96b108a8
JiayangWang <jiayangwQ@usc.edu>
2022-01-27 20:07:10 -0800

    step size 1e2

--- image_unet_opt.py
+++ image_unet_opt.py
@@ -92,10 +92,10 @@ L1Loss = torch.nn.L1Loss()
 #beta = 1e-3
 #ms_ssim_module = MS_SSIM(data_range=255, size_average=True, channel=1)
 
-step = 1e3 # sampling weight optimization step size
+step = 1e2 # sampling weight optimization step size
 
 # %% training
-max_epochs = 20
+max_epochs = 10
 #val_loss = torch.zeros(max_epochs)
 for epoch in range(max_epochs):
     print("epoch:",epoch+1)
