d5247028a99fb9deb7a620c7b6a82c1ca58edeb1
JiayangWang <jiayangwQ@usc.edu>
2022-03-09 13:30:50 -0800

    noise 0.1 low60

--- NN_pytorch/image_unet_low.py
+++ NN_pytorch/image_unet_low.py
@@ -124,6 +124,5 @@ for epoch in range(max_epochs):
         recon_optimizer.step()
         recon_optimizer.zero_grad()
 
-    torch.save(recon_model,"./low60_model_noise"+str(sigma))
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low60_model_noise"+str(sigma))
 
-# %%
