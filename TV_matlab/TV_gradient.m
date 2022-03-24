8012b16513dd1431ede07e0385091aab4161507e
JiayangWang <jiayangwQ@usc.edu>
2022-03-24 15:40:54 -0700

    TV noise2

--- TV_matlab/TV_gradient.m
+++ TV_matlab/TV_gradient.m
@@ -55,7 +55,7 @@ Dh = @(x) reshape(fft2c(reshape(difference_H(x,N1,N2,Nc,d1,d2),N1,N2,Nc)),[],1);
 DhD = reshape(real(Dh(D(ones(N1,N2,Nc)))),N1,N2,Nc);
 
 %% reconstruction parameters initialization
-sigma = 0.1;
+sigma = 0.2;
 noise = complex(sigma*randn(N1,N2,Nc),sigma*randn(N1,N2,Nc));
 factor = 8;
 weight = factor*ones(1,N2);
