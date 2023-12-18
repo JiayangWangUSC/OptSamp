721ec005bfbfae905116dc75767dc99aa9135d28
JiayangWang <jiayangwQ@usc.edu>
2023-12-17 16:15:43 -0800

    low60_L2

--- NN_pytorch/image_unet_low.py
+++ NN_pytorch/image_unet_low.py
@@ -53,8 +53,8 @@ class Sample(torch.nn.Module):
         # low_80(40,317)
         # low_60(80,237)
         # low_40(120,157)
-        noise = noise/math.sqrt(self.factor/317*396)
-        support[torch.arange(40,40+317)] = 1
+        noise = noise/math.sqrt(self.factor/237*396)
+        support[torch.arange(80,80+237)] = 1
         kspace_noise = torch.mul(kspace + noise, support.unsqueeze(0).unsqueeze(1).unsqueeze(3).unsqueeze(0).repeat(kspace.size(0),16,384,1,2))
         return kspace_noise
 
@@ -64,7 +64,7 @@ def toIm(kspace):
 
 # %% sampling
 factor = 8
-sigma = 8
+sigma = 1
 print("noise level:", sigma)
 sample_model = Sample(sigma,factor)
 
@@ -126,5 +126,5 @@ for epoch in range(max_epochs):
         recon_optimizer.step()
         recon_optimizer.zero_grad()
 
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low80_L2_model_sigma"+str(sigma))
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low60_L2_model_sigma"+str(sigma))
 
