460027e905eed8dfcf2622b729d4d6ae931f8c44
JiayangWang <jiayangwQ@usc.edu>
2024-09-16 17:04:33 -0700

    low50 snr10

--- NN_pytorch/image_unet_low.py
+++ NN_pytorch/image_unet_low.py
@@ -8,24 +8,34 @@ from fastmri.data import transforms
 from fastmri.models import Unet
 
 import numpy as np
-import math
 import pathlib
 import torch.optim as optim
 from fastmri.data import  mri_data
+import math
+
+import matplotlib.pyplot as plt
+
+from my_data import *
 
 #from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 # %% data loader
-def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
+N1 = 320
+N2 = 320
+Nc = 20
+def data_transform(kspace,maps):
     # Transform the kspace to tensor format
     kspace = transforms.to_tensor(kspace)
-    image = fastmri.ifft2c(kspace)
-    image = image[:,torch.arange(191,575),:,:]
-    kspace = fastmri.fft2c(image)/5e-5
-    return kspace
+    maps = transforms.to_tensor(maps)
+    kspace = torch.cat((kspace[torch.arange(Nc),:,:].unsqueeze(3),kspace[torch.arange(Nc,2*Nc),:,:].unsqueeze(3)),3)
+    maps = torch.cat((maps[torch.arange(Nc),:,:].unsqueeze(3),maps[torch.arange(Nc,2*Nc),:,:].unsqueeze(3)),3)
+    kspace = kspace.permute([0,2,1,3])
+    maps = maps.permute([0,2,1,3]) + 1e-7
 
-train_data = mri_data.SliceDataset(
-    #root=pathlib.Path('/home/wjy/Project/fastmri_dataset/test/'),
-    root = pathlib.Path('/project/jhaldar_118/jiayangw/dataset/brain/train/'),
+    return kspace, maps
+
+train_data = SliceDataset(
+    #root=pathlib.Path('/home/wjy/Project/fastmri_dataset/brain_T1_demo/'),
+    root = pathlib.Path('/project/jhaldar_118/jiayangw/dataset/brain_T1/multicoil_train/'),
     transform=data_transform,
     challenge='multicoil'
 )
@@ -44,40 +54,47 @@ class Sample(torch.nn.Module):
 
     def __init__(self,sigma,factor):
         super().__init__()
-        self.factor = factor
-        self.sigma = sigma/np.sqrt(2*16)
+        self.mask = factor*torch.ones(N2)
+        self.sigma = sigma
 
     def forward(self,kspace):
         noise = self.sigma*torch.randn_like(kspace)
-        support = torch.zeros(396)
-        # low_80(40,317)
-        # low_60(80,237)
-        # low_40(120,157)
-        noise = noise/math.sqrt(self.factor/157*396)
-        support[torch.arange(120,120+157)] = 1
-        kspace_noise = torch.mul(kspace + noise, support.unsqueeze(0).unsqueeze(1).unsqueeze(3).unsqueeze(0).repeat(kspace.size(0),16,384,1,2))
+        
+        # low_50(80,240)
+        # low_25(120,200)
+        support = torch.zeros(N2)
+        support[torch.arange(80,240)] = 1
+        noise = noise/math.sqrt(factor*2)
+        
+        kspace_noise = torch.mul(kspace + noise, support.unsqueeze(0).unsqueeze(1).unsqueeze(3).unsqueeze(0).repeat(kspace.size(0),Nc,N1,1,2))
+        
         return kspace_noise
 
-def toIm(kspace): 
-    image = fastmri.rss(fastmri.complex_abs(fastmri.ifft2c(kspace)), dim=1)
-    return image
+def toIm(kspace,maps): 
+    # kspace-(batch,Nc,N1,N2,2) maps-(batch,Nc,N1,N2,2)
+    # image-(batch,N1,N2)
+    image = fastmri.complex_abs(torch.sum(fastmri.complex_mul(fastmri.ifft2c(kspace),fastmri.complex_conj(maps)),dim=1))
+    return image.squeeze()
 
 # %% sampling
 factor = 8
-sigma = 4
-print("noise level:", sigma)
+snr = 10
+sigma =  math.sqrt(8)*45/snr
+print("SNR:", snr)
+
 sample_model = Sample(sigma,factor)
+
 
 # %% unet loader
 recon_model = Unet(
-  in_chans = 32,
-  out_chans = 32,
+  in_chans = 40,
+  out_chans = 40,
   chans = 128,
   num_pool_layers = 4,
   drop_prob = 0.0
 )
 
-#recon_model = torch.load('/home/wjy/Project/optsamp_models/uni_model_noise0.3',map_location=torch.device('cpu'))
+#recon_model = torch.load('/project/jhaldar_118/jiayangw/OptSamp/uniform_model_selfloss_noise'+str(sigma))
 
 # %% GPU 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -88,43 +105,54 @@ train_dataloader = torch.utils.data.DataLoader(train_data,batch_size,shuffle=Tru
 sample_model.to(device)
 recon_model.to(device)
 
+
 # %% optimizer
 recon_optimizer = optim.Adam(recon_model.parameters(),lr=3e-4)
+
 #Loss = torch.nn.MSELoss()
 L1Loss = torch.nn.L1Loss()
 L2Loss = torch.nn.MSELoss()
+
 #beta = 1e-3
 #ms_ssim_module = MS_SSIM(data_range=255, size_average=True, channel=1)
 
+
 # %% training
-max_epochs = 50
+max_epochs = 100
 #val_loss = torch.zeros(max_epochs)
 for epoch in range(max_epochs):
     print("epoch:",epoch+1)
     batch_count = 0
-    for train_batch in train_dataloader:
+    
+    for kspace, maps in train_dataloader:
         
         batch_count = batch_count + 1
-        train_batch.to(device)
-        gt = toIm(train_batch)
-        #support = torch.ge(gt,0.05*torch.max(gt))
         
-        kspace_noise = sample_model(train_batch).to(device)
-        image_noise = fastmri.ifft2c(kspace_noise).to(device)
-        image_input = torch.cat((image_noise[:,:,:,:,0],image_noise[:,:,:,:,1]),1).to(device) 
+        
+        gt = toIm(kspace, maps)
+        
+        kspace_noise = sample_model(kspace)
+        image_noise = fastmri.ifft2c(kspace_noise)
+        image_input = torch.cat((image_noise[:,:,:,:,0],image_noise[:,:,:,:,1]),1).to(device)
         image_output = recon_model(image_input).to(device)
-        image_recon = torch.cat((image_output[:,torch.arange(16),:,:].unsqueeze(4),image_output[:,torch.arange(16,32),:,:].unsqueeze(4)),4).to(device)
-        recon = fastmri.rss(fastmri.complex_abs(image_recon), dim=1)
-        #loss = L2Loss(torch.mul(recon.to(device),support.to(device)),torch.mul(gt.to(device),support.to(device))) + beta*L1Loss(torch.mul(recon.to(device),gradmap.to(device)),torch.mul(gt.to(device),gradmap.to(device)))
-        #loss = L1Loss(torch.mul(recon.to(device),support.to(device)),torch.mul(gt.to(device),support.to(device)))
-        loss = L2Loss(recon.to(device),gt.to(device))
+        image_recon = torch.cat((image_output[:,torch.arange(Nc),:,:].unsqueeze(4),image_output[:,torch.arange(Nc,2*Nc),:,:].unsqueeze(4)),4).to(device)
+        
+        recon = fastmri.complex_abs(torch.sum(fastmri.complex_mul(image_recon,fastmri.complex_conj(maps.to(device))),dim=1)).squeeze()
 
-        if batch_count%100 == 0:
+
+        loss = L1Loss(recon.to(device),gt.to(device))
+
+        if batch_count%10 == 0:
             print("batch:",batch_count,"train loss:",loss.item())
         
+
         loss.backward()
+
         recon_optimizer.step()
         recon_optimizer.zero_grad()
 
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low40_L2_model_sigma"+str(sigma))
+   torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low50_mae_snr"+str(snr))
 
+
+
+# %%
