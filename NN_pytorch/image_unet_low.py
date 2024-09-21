879b2510f7be16d73454f71ffa32ee4d50d6b0f0
JiayangWang <jiayangwQ@usc.edu>
2024-09-20 23:00:53 -0700

    low25 mse snr10

--- NN_pytorch/image_unet_low.py
+++ NN_pytorch/image_unet_low.py
@@ -78,7 +78,7 @@ def toIm(kspace,maps):
 
 # %% sampling
 factor = 8
-snr = 3
+snr = 10
 sigma =  math.sqrt(8)*45/snr
 print("SNR:", snr)
 print('low25')
@@ -95,7 +95,7 @@ recon_model = Unet(
   drop_prob = 0.0
 )
 
-recon_model = torch.load('/project/jhaldar_118/jiayangw/OptSamp/model/low25_mae_snr'+str(snr))
+recon_model = torch.load('/project/jhaldar_118/jiayangw/OptSamp/model/low25_mse_snr'+str(snr))
 
 # %% GPU 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -141,10 +141,10 @@ for epoch in range(max_epochs):
         recon = fastmri.complex_abs(torch.sum(fastmri.complex_mul(image_recon,fastmri.complex_conj(maps.to(device))),dim=1)).squeeze()
 
 
-        loss = L1Loss(recon.to(device),gt.to(device))
+        loss = L2Loss(recon.to(device),gt.to(device))
 
         if batch_count%10 == 0:
-            print("batch:",batch_count,"L1 loss:",loss.item())
+            print("batch:",batch_count,"L2 loss:",loss.item())
         
 
         loss.backward()
@@ -152,7 +152,7 @@ for epoch in range(max_epochs):
         recon_optimizer.step()
         recon_optimizer.zero_grad()
 
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low25_mae_snr"+str(snr))
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/low25_mse_snr"+str(snr))
 
 
 
