4053cb89ccc4e3e1f3591975a3aa056eec61fbfb
JiayangWangUSC <jiayangw@usc.edu>
2024-09-25 19:42:51 -0700

    preprocess val

--- TV_matlab/preprocess.sh
+++ TV_matlab/preprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=16GB
-#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --time=6:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jiayangw@usc.edu
 #SBATCH --account=jhaldar_118
