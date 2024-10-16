94c0632355c5fead9042a16e4b7790b7380b3f38
JiayangWangUSC <jiayangw@usc.edu>
2024-10-15 17:18:26 -0700

    step3

--- NN_pytorch/image_unet_opt.py
+++ NN_pytorch/image_unet_opt.py
@@ -81,7 +81,7 @@ def toIm(kspace,maps):
 
 # %% sampling
 factor = 8
-snr = 10
+snr = 5
 sigma =  0.15*math.sqrt(8)/snr
 print("SNR:", snr, flush = True)
 print('opt step3', flush = True)
--- NN_pytorch/image_unet_uniform.py
+++ NN_pytorch/image_unet_uniform.py
@@ -72,7 +72,7 @@ def toIm(kspace,maps):
 
 # %% sampling
 factor = 8
-snr = 3
+snr = 10
 sigma =  0.15*math.sqrt(8)/snr
 print("SNR:", snr, flush = True)
 print('uniform', flush = True)
