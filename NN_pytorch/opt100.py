826a56a8371d8851b13c8067771197c4ea7d0648
JiayangWang <jiayangwQ@usc.edu>
2024-10-16 13:38:08 -0700

    mse snr10

--- NN_pytorch/opt100.py
+++ NN_pytorch/opt100.py
@@ -19,6 +19,9 @@ from my_data import *
 
 #from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 # %% data loader
+snr = 10
+print("SNR:", snr, flush = True)
+
 N1 = 320
 N2 = 320
 Nc = 16
@@ -49,7 +52,7 @@ val_data = SliceDataset(
 
 # %% noise generator and transform to image
 batch_size = 8
-
+print('opt100', flush = True)
 class Sample(torch.nn.Module): 
 
     def __init__(self,sigma,factor):
@@ -81,10 +84,9 @@ def toIm(kspace,maps):
 
 # %% sampling
 factor = 8
-snr = 3
 sigma =  0.15*math.sqrt(8)/snr
-print("SNR:", snr, flush = True)
-print('opt step3', flush = True)
+
+
 
 sample_model = Sample(sigma,factor)
 
@@ -97,7 +99,7 @@ recon_model = Unet(
   drop_prob = 0.0
 )
 
-#recon_model = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/basemodel")
+recon_model = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/uni100_mse_snr"+str(snr))
 
 # %% data loader
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -114,63 +116,91 @@ print('L2 Loss', flush = True)
 #Loss = torch.nn.L1Loss()
 Loss = torch.nn.MSELoss()
 
-step = 3
+step = 1
 
 # %% training
 max_epochs = 50
 for epoch in range(max_epochs):
     print("epoch:",epoch+1)
-    step = 0.9 * step
+    if epoch < 20:
+        step = 0.9 * step
+        trainloss = 0
+        trainloss_normalized = 0
+        for kspace, maps in train_dataloader:
+            sample_model.weight.requires_grad = True
 
-    trainloss = 0
-    trainloss_normalized = 0
-    for kspace, maps in train_dataloader:
-        sample_model.weight.requires_grad = True
-
-        gt = toIm(kspace, maps) # ground truth
-        support = fastmri.complex_abs(torch.sum(fastmri.complex_mul(maps,fastmri.complex_conj(maps)),dim=1))
-        kspace_noise = sample_model(kspace) # add noise
+            gt = toIm(kspace, maps) # ground truth
+            support = fastmri.complex_abs(torch.sum(fastmri.complex_mul(maps,fastmri.complex_conj(maps)),dim=1))
+            kspace_noise = sample_model(kspace) # add noise
         
-        # forward
-        image_noise = fastmri.ifft2c(kspace_noise)
-        image_input = torch.cat((image_noise[:,:,:,:,0],image_noise[:,:,:,:,1]),1).to(device)
-        image_output = recon_model(image_input).to(device)
-        recon = fastmri.complex_abs(torch.cat((image_output[:,0,:,:].unsqueeze(1).unsqueeze(4),image_output[:,1,:,:].unsqueeze(1).unsqueeze(4)),4)).squeeze().to(device)
-        recon = recon * support.to(device)
+            # forward
+            image_noise = fastmri.ifft2c(kspace_noise)
+            image_input = torch.cat((image_noise[:,:,:,:,0],image_noise[:,:,:,:,1]),1).to(device)
+            image_output = recon_model(image_input).to(device)
+            recon = fastmri.complex_abs(torch.cat((image_output[:,0,:,:].unsqueeze(1).unsqueeze(4),image_output[:,1,:,:].unsqueeze(1).unsqueeze(4)),4)).squeeze().to(device)
+            recon = recon * support.to(device)
 
-        loss = Loss(recon.to(device),gt.to(device))
-        trainloss += loss.item()
-        trainloss_normalized += loss.item()/Loss(0*gt,gt)
+            loss = Loss(recon.to(device),gt.to(device))
+            trainloss += loss.item()
+            trainloss_normalized += loss.item()/Loss(0*gt,gt)
 
-        # backward
-        loss.backward()
+            # backward
+            loss.backward()
         
-        # optimize mask
-        with torch.no_grad():
+            # optimize mask
+            with torch.no_grad():
 
-            weight = sample_model.weight.clone() 
-            ind = torch.where(weight >= 1)[0]
+                weight = sample_model.weight.clone() 
+                ind = torch.where(weight >= 1)[0]
 
-            grad = sample_model.weight.grad
-            temp = grad[ind]
-            temp = temp - temp.mean()
-            temp = temp/temp.norm()
-            grad = 0 * grad
-            grad[ind] = temp
+                grad = sample_model.weight.grad
+                temp = grad[ind]
+                temp = temp - temp.mean()
+                temp = temp/temp.norm()
+                grad = 0 * grad
+                grad[ind] = temp
 
-            weight = weight - step * grad
-            weight[weight<1] = 1e-7
-            ind = torch.where(weight >= 1)[0]
+                weight = weight - step * grad
+                weight[weight<1] = 1e-7
+                ind = torch.where(weight >= 1)[0]
             
-            temp = weight[ind] - 1
-            temp = temp/temp.mean()*(factor*N2/len(ind)-1)
-            weight[ind] = temp + 1
+                temp = weight[ind] - 1
+                temp = temp/temp.mean()*(factor*N2/len(ind)-1)
+                weight[ind] = temp + 1
 
-            sample_model.weight = weight
+                sample_model.weight = weight
         
-        # optimize network
-        recon_optimizer.step()
-        recon_optimizer.zero_grad()
+            # optimize network
+            recon_optimizer.step()
+            recon_optimizer.zero_grad()
+
+            print("weight max:",weight.max(),"min:",temp.min()+1, "nonzero:", temp.size(0), flush = True)
+
+    else:
+        sample_model.weight.requires_grad = False
+        trainloss = 0
+        trainloss_normalized = 0
+        for kspace, maps in train_dataloader:
+
+            gt = toIm(kspace, maps) # ground truth
+            support = fastmri.complex_abs(torch.sum(fastmri.complex_mul(maps,fastmri.complex_conj(maps)),dim=1))
+            kspace_noise = sample_model(kspace) # add noise
+        
+            # forward
+            image_noise = fastmri.ifft2c(kspace_noise)
+            image_input = torch.cat((image_noise[:,:,:,:,0],image_noise[:,:,:,:,1]),1).to(device)
+            image_output = recon_model(image_input).to(device)
+            recon = fastmri.complex_abs(torch.cat((image_output[:,0,:,:].unsqueeze(1).unsqueeze(4),image_output[:,1,:,:].unsqueeze(1).unsqueeze(4)),4)).squeeze().to(device)
+            recon = recon * support.to(device)
+
+            loss = Loss(recon.to(device),gt.to(device))
+            trainloss += loss.item()
+            trainloss_normalized += loss.item()/Loss(0*gt,gt)
+
+            # backward
+            loss.backward()
+            recon_optimizer.step()
+            recon_optimizer.zero_grad()
 
     with torch.no_grad():
         valloss = 0
@@ -193,11 +223,8 @@ for epoch in range(max_epochs):
 
     print("train loss:",trainloss/331/8," val loss:",valloss/42/8, flush = True)
     print("normalized train loss:",trainloss_normalized/331/8," normalized val loss:",valloss_normalized/42/8, flush = True)
-
-    print("weight max:",weight.max(),"min:",weight.min(),"mean:", weight.mean(), flush = True)
-
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/opt_step3_mse_snr"+str(snr))
-    torch.save(weight,"/project/jhaldar_118/jiayangw/OptSamp/model/opt_step3_mse_mask_snr"+str(snr))
-
+    
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/OptSamp/model/opt100_mse_snr"+str(snr))
+    torch.save(weight,"/project/jhaldar_118/jiayangw/OptSamp/model/opt100_mse_mask_snr"+str(snr))
 
 # %%
