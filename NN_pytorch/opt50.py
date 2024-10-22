b51657f5097466443a9bfae77bdf3ba2206d0913
JiayangWang <jiayangwQ@usc.edu>
2024-10-22 16:08:30 -0700

    snr 20

--- NN_pytorch/opt50.py
+++ NN_pytorch/opt50.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 from my_data import *
 
 # %% data loader
-snr = 2
+snr = 20
 print("SNR:", snr, flush = True)
 
 N1 = 320
@@ -96,7 +96,7 @@ recon_model = Unet(
   drop_prob = 0.0
 )
 
-recon_model = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/uni50_mse_snr"+str(snr))
+recon_model = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/opt50_mae_snr"+str(10))
 #weight = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/opt50_mse_mask_snr"+str(snr))
 #sample_model.weight = weight
 
