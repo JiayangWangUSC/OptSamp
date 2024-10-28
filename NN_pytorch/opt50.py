015e13fb37617d5badf3bb1babfa7109d48e6336
JiayangWang <jiayangwQ@usc.edu>
2024-10-27 17:09:03 -0700

    snr 30

--- NN_pytorch/opt50.py
+++ NN_pytorch/opt50.py
@@ -6,19 +6,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 from fastmri.data import transforms
 from fastmri.models import Unet
-
 import numpy as np
 import pathlib
 import torch.optim as optim
 from fastmri.data import  mri_data
 import math
-
 import matplotlib.pyplot as plt
-
 from my_data import *
 
 # %% data loader
-snr = 20
+snr = 30
 print("SNR:", snr, flush = True)
 
 N1 = 320
@@ -99,8 +96,8 @@ recon_model = Unet(
   drop_prob = 0.0
 )
 
-recon_model = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/opt50_mae_snr"+str(snr))
-weight = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/opt50_mae_mask_snr"+str(snr))
+recon_model = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/opt50_mse_snr"+str(snr))
+weight = torch.load("/project/jhaldar_118/jiayangw/OptSamp/model/opt50_mse_mask_snr"+str(snr))
 sample_model.weight = weight
 
 # %% data loader
