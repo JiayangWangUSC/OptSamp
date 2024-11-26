2ef78253d267cd93e6ea110bcb02532656373ba4
JiayangWang <jiayangwQ@usc.edu>
2024-11-26 15:44:58 -0800

    snr 20 resolution 1

--- NN_pytorch/uni.py
+++ NN_pytorch/uni.py
@@ -61,16 +61,14 @@ class Sample(torch.nn.Module):
 
     def __init__(self,sigma,factor):
         super().__init__()
-        self.mask =  torch.zeros((N1,N2))
-        self.mask[(5*reso):(N1-5*reso),(5*reso):(N2-5*reso)] = 1/(factor*N1/(N1-10*reso)*N2/(N2-10*reso))
+        self.weight =  torch.zeros((N1,N2))
+        self.weight[(5*reso):(N1-5*reso),(5*reso):(N2-5*reso)] = 1/(factor*N1/(N1-10*reso)*N2/(N2-10*reso))
         self.sigma = sigma
 
     def forward(self,kspace):
         noise = self.sigma*torch.randn_like(kspace)
-        print(self.mask.size())
-        print(torch.sqrt(self.mask).unsqueeze(0).unsqueeze(0).unsqueeze(4).size())
-        self.mask = torch.sqrt(self.mask).unsqueeze(0).unsqueeze(0).unsqueeze(4).repeat(kspace.size(0),Nc,1,1,2) 
-        kspace_noise = (self.mask>0) * kspace + noise * self.mask
+        mask = torch.sqrt(self.weight).unsqueeze(0).unsqueeze(0).unsqueeze(4).repeat(kspace.size(0),Nc,1,1,2) 
+        kspace_noise = (mask>0) * kspace + noise * mask
         return kspace_noise
 
 def toIm(kspace,maps): 
