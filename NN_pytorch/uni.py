3ecaf56cee5400efabfca7d0fa91da3c5d9232a5
JiayangWang <jiayangwQ@usc.edu>
2024-11-29 15:58:04 -0800

    snr 10 resolution 1

--- NN_pytorch/uni.py
+++ NN_pytorch/uni.py
@@ -20,7 +20,7 @@ from my_data import *
 #from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 # %% data loader
 snr = 10
-reso = 0
+reso = 1
 print("SNR:", snr, flush = True)
 print('resolution:', reso, flush = True)
 
