cad1d4744313f9b678802ca058a5780712dea907
JiayangWang <jiayangwQ@usc.edu>
2024-12-02 12:24:02 -0800

    snr 3 resolution 8

--- NN_pytorch/uni.py
+++ NN_pytorch/uni.py
@@ -20,7 +20,7 @@ from my_data import *
 #from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 # %% data loader
 snr = 3
-reso = 7
+reso = 8
 print("SNR:", snr, flush = True)
 print('resolution:', reso, flush = True)
 
