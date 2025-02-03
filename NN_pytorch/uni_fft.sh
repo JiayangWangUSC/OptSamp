dff4832f52d553e3ab185f42fdff517172ca5793
JiayangWang <jiayangwQ@usc.edu>
2025-02-03 13:35:06 -0800

    snr 3 reso 3

--- NN_pytorch/uni_fft.sh
+++ NN_pytorch/uni_fft.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=12GB
 #SBATCH --time=24:00:00
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type= END,FAIL
 #SBATCH --mail-user=jiayangw@usc.edu
 #SBATCH --account=jhaldar_118
 
