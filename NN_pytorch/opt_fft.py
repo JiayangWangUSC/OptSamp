2d63049b046b485f7f131758a844c3ef442ba3f5
JiayangWang <jiayangwQ@usc.edu>
2025-02-11 15:30:14 -0800

    snr2

--- NN_pytorch/opt_fft.py
+++ NN_pytorch/opt_fft.py
@@ -156,7 +156,10 @@ for epoch in range(max_epochs):
                 grad = sample_model.weight.grad
                 grad = grad - grad.mean()
                 grad = grad/grad.norm()
+                total = weight2.sum()
                 weight2 = weight2 - step2 * grad
+                weight2[weight2 < 1] = 1
+                weight2 = weight2 / weight2.sum * total
                 sample_model.weight = weight2
                 print("sampling weight max:", weight2.max(), " min:", weight2.min(), flush = True)
 
