fbf234e5c6b6aa5f91b6b63f0b1ece5a5ddab3c6
JiayangWang <jiayangwQ@usc.edu>
2025-02-13 13:05:33 -0800

    snr5

--- NN_pytorch/figure_fft.py
+++ NN_pytorch/figure_fft.py
@@ -15,16 +15,13 @@ import matplotlib.pyplot as plt
 from my_data import *
 
 # %% data loader
-snr = 5
+snr = 2
 reso = 5
-print('uni fft')
-print("SNR:", snr, flush = True)
-print('resolution:', reso, flush = True)
 
 N1 = 320
 N2 = 320
 Nc = 16
- 
+
 def data_transform(kspace,maps):
     # Transform the kspace to tensor format
     kspace = transforms.to_tensor(kspace)
@@ -32,26 +29,19 @@ def data_transform(kspace,maps):
     kspace = torch.cat((kspace[torch.arange(Nc),:,:].unsqueeze(3),kspace[torch.arange(Nc,2*Nc),:,:].unsqueeze(3)),3)
     maps = torch.cat((maps[torch.arange(Nc),:,:].unsqueeze(3),maps[torch.arange(Nc,2*Nc),:,:].unsqueeze(3)),3)
     kspace = kspace.permute([0,2,1,3])
-    maps = maps.permute([0,2,1,3])
+    maps = maps.permute([0,2,1,3]) 
 
     return kspace, maps
 
-train_data = SliceDataset(
-    #root=pathlib.Path('/home/wjy/Project/fastmri_dataset/brain_T1_demo/'),
-    root = pathlib.Path('/project/jhaldar_118/jiayangw/dataset/brain_T1/multicoil_val/'),
-    transform=data_transform,
-    challenge='multicoil'
-)
-
-val_data = SliceDataset(
-    #root=pathlib.Path('/home/wjy/Project/fastmri_dataset/brain_T1_demo/'),
-    root = pathlib.Path('/project/jhaldar_118/jiayangw/dataset/brain_T1/multicoil_val/'),
+test_data = SliceDataset(
+    root=pathlib.Path('/home/wjy/Project/fastmri_dataset/brain_T1_demo/'),
+    #root = pathlib.Path('/project/jhaldar_118/jiayangw/dataset/brain_T1/multicoil_train/'),
     transform=data_transform,
     challenge='multicoil'
 )
 
 # %% noise generator and transform to image
-batch_size = 8
+batch_size = 1
 
 class Sample(torch.nn.Module): 
 
@@ -65,22 +55,24 @@ class Sample(torch.nn.Module):
         mask =  torch.zeros((N1,N2))
         mask[(16*reso):(N1-16*reso),(16*reso):(N2-16*reso)] =  1.0 / (self.weight ** 0.5).unsqueeze(0).repeat(N1-32*reso,1)
         mask = mask.unsqueeze(0).unsqueeze(0).unsqueeze(4).repeat(kspace.size(0),Nc,1,1,2)
+        
         noise = self.sigma * torch.randn_like(kspace)
-        kspace_noise =  (mask>0) * kspace + mask * noise 
+        kspace_noise =  (mask > 0) * kspace + mask * noise 
+
         return kspace_noise
     
 class Recon(torch.nn.Module): 
-
+    
     def __init__(self):
         super().__init__()
-        self.weight = 0.5 * torch.ones(N1-32*reso, N2-32*reso)
+        self.weight =  0.01 * torch.ones(N1-32*reso, N2-32*reso)
 
     def forward(self,kspace):
-
         mask =  torch.zeros((N1,N2))
         mask[(16*reso):(N1-16*reso),(16*reso):(N2-16*reso)] =  self.weight
         mask = mask.unsqueeze(0).unsqueeze(0).unsqueeze(4).repeat(kspace.size(0),Nc,1,1,2)
         kspace =  mask * kspace 
+
         return kspace
 
 def toIm(kspace,maps): 
@@ -93,60 +85,37 @@ def toIm(kspace,maps):
 
 # %% sampling
 factor = 8
-sigma =  0.12*math.sqrt(8)/snr
+sigma =  0.12 * math.sqrt(8) / snr
 
+weight1 = torch.load('/home/wjy/Project/optsamp_model/opt_mask_window_snr'+str(snr)+'_reso'+str(reso))
 sample_model = Sample(sigma,factor)
+sample_model.weight = weight1
+
+weight2 = torch.load('/home/wjy/Project/optsamp_model/opt_window_snr'+str(snr)+'_reso'+str(reso))
 recon_model = Recon()
+recon_model.weight = weight2
 
 # %% data loader
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-train_dataloader = torch.utils.data.DataLoader(train_data,batch_size,shuffle=True)
-val_dataloader = torch.utils.data.DataLoader(val_data,batch_size,shuffle=True)
-
-sample_model.to(device)
-recon_model.to(device)
+test_dataloader = torch.utils.data.DataLoader(test_data,batch_size,shuffle=True)
 
 # %% optimization parameters
 Loss = torch.nn.MSELoss()
-step = 3000
 
-# %% training
-max_epochs = 100
-for epoch in range(max_epochs):
-    print("epoch:",epoch+1)
+from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+ssim_module = SSIM(data_range=255, size_average=True, channel=1)
 
-    step = 0.99 * step
-    #trainloss = 0
-    #trainloss_normalized = 0
-    for kspace, maps in train_dataloader:
-        
-        recon_model.weight.requires_grad = True
-        gt = toIm(kspace, maps) # ground truth
+# %% test
+with torch.no_grad():
+    kspace, maps =  test_data[0]
+    kspace = kspace.unsqueeze(0)
+    maps = maps.unsqueeze(0)
 
-        kspace_noise = recon_model(sample_model(kspace)) # add noise and apply window
-        recon = toIm(kspace_noise, maps)
-        loss = Loss(recon.to(device),gt.to(device))
-        #trainloss += loss.item()
-        #trainloss_normalized += loss.item()/Loss(0*gt,gt)
+    gt = toIm(kspace, maps) # ground truth
+    scale = gt.max()
+    l2scale = gt.norm(p=2)
 
-        # backward
-        loss.backward()
-        
-        # optimize mask
-        with torch.no_grad():
-            weight = recon_model.weight.clone() 
-            grad = recon_model.weight.grad
-            
-            weight = weight - step * grad
-            weight[weight > 1] = 1
-            weight[weight < 0] = 0
-
-            recon_model.weight = weight
-        
-        print("weight max:",weight.max(),"weight min:",weight.min(), flush = True)
-        print("Loss:", loss.item() ,flush = True)
-
-    torch.save(weight,"/project/jhaldar_118/jiayangw/OptSamp/model/uni_window_snr"+str(snr)+"_reso"+str(reso))
-
+    kspace_noise = recon_model(sample_model(kspace)) # add noise and apply window
+    recon = toIm(kspace_noise, maps)
+    
 # %%
